99b54f298c7452462344b961e629a8b6c0544f6f
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-03-25 12:31:56 +0800

    Update: add pricing calculator

---
+++ lib/utils/helpers/pricing_calculator.dart
@@ -0,0 +1,39 @@
+class PricingCalculator {
+
+  //Calculate price based on tax and shipping
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shippingCost = getShippingCost(location);
+
+    double totalPrice = productPrice + taxAmount + shippingCost;
+    return totalPrice;
+  }
+
+  //Calculate shipping cost
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  //Calculate tax
+  static String calculateTax(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location) {
+    return 0.10; //lookup the tax rate for the location from tax rate db/API
+  }
+
+  static double getShippingCost(String location) {
+    return 5.00; //lookup the shipping cost for the location using API
+  }
+
+  //Sum all cart values and return total amount
+  // static double calculateCartTotal(CartModel cart) {
+  //   return cart.items.map((e) => e.price).fold(0, (previousPrice, currentPrice) => previousPrice + (currentPrice ?? 0));
+  // }
+}
