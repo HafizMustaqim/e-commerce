7ca54bbc741a96ffb072eabdb72707be851e8a5f
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-04-01 16:42:13 +0800

    Update: On boarding frontend

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,4 +1,4 @@
-class onstantImages {
+class constantImages {
   
   //App logo
   static const String darkAppLogo = "";
@@ -7,5 +7,10 @@ class onstantImages {
   //Social logo
   static const String google = "assets/logos/google.svg";
   static const String facebook = "assets/logos/facebook.svg";
+
+  //OnBoarding Images
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/searching.gif";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/shopping.gif";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/delivery.gif";
 
 }
--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -5,7 +5,7 @@ class ConstantTexts {
   //OnBoarding Texts
   static const String onBoardingTitle1 = "Choose your product";
   static const String onBoardingTitle2 = "Select Payment Method";
-  static const String onBoardingTitle3 = "Delive at your door step";
+  static const String onBoardingTitle3 = "Deliver at your door step";
 
   static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
   static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - your Convenience, Our Priority!";
