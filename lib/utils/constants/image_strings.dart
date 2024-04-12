e66e3793c321c9bc5aeee41eb9398260ea5ed8aa
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-04-12 11:15:43 +0800

    Update: Image Assets

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -9,8 +9,8 @@ class ConstantImages {
   static const String facebook = "assets/logos/facebook.png";
 
   //OnBoarding Images
-  static const String onBoardingImage1 = "assets/images/on_boarding_images/shopping.gif";
-  static const String onBoardingImage2 = "assets/images/on_boarding_images/searching.gif";
-  static const String onBoardingImage3 = "assets/images/on_boarding_images/delivery.gif";
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/sammy-line-woman-in-shopping-cart-placing-order-on-smartphone.gif";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-line-sailor-on-mast-looking-through-telescope.gif";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/sammy-line-man-and-dog-delivering-packages-on-a-moped.gif";
 
 }
