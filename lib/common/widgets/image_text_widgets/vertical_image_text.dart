57196031cd8101b6f89f31e6d06c4992bf564ee8
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-04-18 19:21:21 +0800

    Fix: remove BGColor constantColors

--- lib/common/widgets/image_text_widgets/vertical_image_text.dart
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -9,7 +9,7 @@ class VerticalImageText extends StatelessWidget {
     required this.image,
     required this.title,
     this.textColor = ConstantColors.white,
-    this.backgroundColor = ConstantColors.white,
+    this.backgroundColor,
     this.onTap,
   });
 
