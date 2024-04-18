17b42cc0baea751123ec6d346244e5a75f0ee879
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-04-18 15:51:07 +0800

    custom searchbar

---
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -0,0 +1,41 @@
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/device/device_utility.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class SearchContainer extends StatelessWidget {
+  const SearchContainer({
+    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true, this.showBorder = true,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunctions.isDarkMode(context);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: ConstantSizes.cardRadiusLg),
+      child: Container(
+        width: DeviceUtils.getScreenWidth(context),
+        padding: const EdgeInsets.all(ConstantSizes.md),
+        decoration: BoxDecoration(
+          color: showBackground ? dark ? ConstantColors.dark : ConstantColors.light : Colors.transparent,
+          borderRadius: BorderRadius.circular(ConstantSizes.cardRadiusLg),
+          border: showBorder ? Border.all(color: ConstantColors.grey) : null,
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: ConstantColors.darkGrey),
+            const SizedBox(width: ConstantSizes.spaceBetweenItems),
+            Text(text, style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:e_commerce/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,7 +16,16 @@ class HomeScreen extends StatelessWidget {
             PrimaryHeaderContainer(
               child: Column(
                 children: [
+                  //Appbar
                   HomeAppBar(),
+                  SizedBox(height: ConstantSizes.spaceBetweenSections),
+
+                  //Searchbar
+                  SearchContainer(text: 'Search in Store'),
+                  SizedBox(height: ConstantSizes.spaceBetweenSections),
+
+                  //Categories
+
                 ],
               ),
             ),
