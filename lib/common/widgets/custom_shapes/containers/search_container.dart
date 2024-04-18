5c4472c3003fc8deb0d25c5f0b18b2fb2cff6dd8
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-04-18 19:01:34 +0800

    Update search_container.dart

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -7,33 +7,42 @@ import 'package:iconsax/iconsax.dart';
 
 class SearchContainer extends StatelessWidget {
   const SearchContainer({
-    super.key, required this.text, this.icon = Iconsax.search_normal, this.showBackground = true, this.showBorder = true,
+    super.key, 
+    required this.text, 
+    this.icon = Iconsax.search_normal, 
+    this.showBackground = true, 
+    this.showBorder = true,
+    this.onTap,
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     final dark = HelperFunctions.isDarkMode(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: ConstantSizes.cardRadiusLg),
-      child: Container(
-        width: DeviceUtils.getScreenWidth(context),
-        padding: const EdgeInsets.all(ConstantSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground ? dark ? ConstantColors.dark : ConstantColors.light : Colors.transparent,
-          borderRadius: BorderRadius.circular(ConstantSizes.cardRadiusLg),
-          border: showBorder ? Border.all(color: ConstantColors.grey) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: ConstantColors.darkGrey),
-            const SizedBox(width: ConstantSizes.spaceBetweenItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: ConstantSizes.cardRadiusLg),
+        child: Container(
+          width: DeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(ConstantSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground ? dark ? ConstantColors.dark : ConstantColors.light : Colors.transparent,
+            borderRadius: BorderRadius.circular(ConstantSizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: ConstantColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: ConstantColors.darkGrey),
+              const SizedBox(width: ConstantSizes.spaceBetweenItems),
+              Text(text, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
