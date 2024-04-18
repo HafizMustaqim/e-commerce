508aa9a37dd12bcded50abf8b4cecbd737597597
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-04-18 12:03:01 +0800

    fix: custom appbar

--- lib/common/widgets/custom_shapes/containers/primary_header_container.dart
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -23,6 +23,7 @@ class PrimaryHeaderContainer extends StatelessWidget {
               //Background Custom Shapes
               Positioned(top: -150, right: -250, child: CircularContainer(backgroundColor: ConstantColors.textWhite.withOpacity(0.1))),
               Positioned(top: 100, right: -300, child: CircularContainer(backgroundColor: ConstantColors.textWhite.withOpacity(0.1))),
+              child,
             ],
           ),
         ),
