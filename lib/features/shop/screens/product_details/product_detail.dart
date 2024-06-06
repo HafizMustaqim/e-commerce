2c45dfb4e571484c5e1df6cca8cc529e67e31184
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-06-06 17:23:07 +0800

    Update: Rating & Share

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,4 +1,6 @@
 import "package:e_commerce/features/shop/screens/product_details/widgets/product_detail_image_slider.dart";
+import "package:e_commerce/features/shop/screens/product_details/widgets/rating_share_widget.dart";
+import "package:e_commerce/utils/constants/sizes.dart";
 import "package:flutter/material.dart";
 
 class ProductDetailScreen extends StatelessWidget {
@@ -11,11 +13,24 @@ class ProductDetailScreen extends StatelessWidget {
         child: Column(
           children: [
             //Product Image Slider
-            ProductImageSlider()
+            ProductImageSlider(),
 
             //Product Details
+            Padding(
+              padding: EdgeInsets.only(right: ConstantSizes.defaultSpace, left: ConstantSizes.defaultSpace, bottom: ConstantSizes.defaultSpace),
+              child: Column(
+                children: [
+                  //Rating & Share
+                  RatingAndShare(),
 
-
+                  //Price, Title, Stock & Brand
+                  //Attributes
+                  //Checkout Button
+                  //Description
+                  //Reviews
+                ],
+              ),
+            ),
           ],
         ),
       )
