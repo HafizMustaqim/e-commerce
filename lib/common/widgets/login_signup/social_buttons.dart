8bd22184329471b9b3b5e2f82f2bff42e7f55624
Hafiz Mustaqim <84282907+HafizMustaqim@users.noreply.github.com>
2024-07-30 11:03:48 +0800

    Update: google sign in

--- lib/common/widgets/login_signup/social_buttons.dart
+++ lib/common/widgets/login_signup/social_buttons.dart
@@ -1,7 +1,9 @@
+import 'package:e_commerce/features/authentication/controllers/login/login_controller.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SocialButtons extends StatelessWidget {
   const SocialButtons({
@@ -10,13 +12,14 @@ class SocialButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
           decoration: BoxDecoration(border: Border.all(color: ConstantColors.grey), borderRadius: BorderRadius.circular(100)),
           child: IconButton(
-            onPressed: () {},
+            onPressed: () => controller.googleSignIn(),
             icon: const Image(
               width: ConstantSizes.iconMd,
               height: ConstantSizes.iconMd,
